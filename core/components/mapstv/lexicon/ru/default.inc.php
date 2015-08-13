3f2e896f864117f22c1f9699d4e8a4ca6629655f
valeriavg <valeria.viana.gusmao@gmail.com>
2015-08-13 22:39:40 +0300

    Russian translation

---
+++ core/components/mapstv/lexicon/ru/default.inc.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * MapsTv
+ *
+ * Copyright 2012 by Wieger Sloot <modx+mapstv@sterc.nl>
+ *
+ * MapsTv is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * MapsTv is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * MapsTv; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package MapsTv
+ */
+/**
+ * Input TV render for MapsTv's MapsTvItem TV
+ *
+ * @package MapsTv
+ * @subpackage tv
+ */
+$_lang['mapstv'] = 'Карта';
+
+$_lang['mapstv.street'] = 'Улица';
+$_lang['mapstv.housenumber'] = 'Дом';
+$_lang['mapstv.zipcode'] = 'Индекс';
+$_lang['mapstv.city'] = 'Город';
+$_lang['mapstv.state'] = 'Штат/Область';
+$_lang['mapstv.country'] = 'Страна';
+
+$_lang['mapstv.generate'] = 'Получить координаты и поставить маркер на карту';
+
+$_lang['mapstv.latitude'] = 'Широта';
+$_lang['mapstv.longitude'] = 'Долгота';
+
+$_lang['mapstv.error'] = 'Ошибка геолокации';
+$_lang['mapstv.error_msg'] = 'Точность определения местоположения невелика. Проверьте маркер и при необходимости переставьте';
