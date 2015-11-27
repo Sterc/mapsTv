6a415f1fcc0f783537300ec65e207146b83972ba
Roel Zeilstra <roel@sterc.nl>
2015-11-27 10:48:27 +0100

    Adding czech lexicon

---
+++ core/components/mapstv/lexicon/cz/default.inc.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * MapsTv
+ *
+ * Copyright 2012 by Wieger Sloot <modx+mapstv@sterc.nl>
+ * Translation: Filip Novak <filip.f.novak@gmail.com>
+ *
+ * MapsTv is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * MapsTv is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * MapsTv; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package MapsTv
+ */
+/**
+ * Input TV render for MapsTv's MapsTvItem TV
+ *
+ * @package MapsTv
+ * @subpackage tv
+ */
+$_lang['mapstv'] = 'Mapy Tv';
+
+$_lang['mapstv.street'] = 'Ulice';
+$_lang['mapstv.housenumber'] = 'Číslo popisné';
+$_lang['mapstv.zipcode'] = 'PSČ';
+$_lang['mapstv.city'] = 'Město';
+$_lang['mapstv.state'] = 'Stát/Kraj/Okres';
+$_lang['mapstv.country'] = 'Země';
+
+$_lang['mapstv.generate'] = 'Vytvořit GPS pozici a zobrazit značku na mapě';
+
+$_lang['mapstv.latitude'] = 'Zeměpisná šířka (latitude)';
+$_lang['mapstv.longitude'] = 'Zeměpisná délka (longitude)';
+
+$_lang['mapstv.error'] = 'Chyba při získávání GPS pozice';
+$_lang['mapstv.error_msg'] = 'Adresa není přesná. Zkontrolujte a případně upravte pozici značky na mapě.';
