3285017919e6f6aee3ce34791b3babb5baa58f4d
alessandro chinazzo <alessandro.chinazzo@gmail.com>
2021-11-24 17:38:39 +0100

    Italian lexicon

---
+++ core/components/mapstv/lexicon/it/default.inc.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * MapsTv
+ *
+ * Copyright 2016 by Sterc <modx+mapstv@sterc.nl>
+ *
+ * MapsTv is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * MapsTv is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * MapsTv; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package MapsTv
+ */
+/**
+ * Input TV render for MapsTv's MapsTvItem TV
+ *
+ * @package MapsTv
+ * @subpackage tv
+ */
+$_lang['mapstv'] = 'Maps Tv';
+
+$_lang['mapstv.street'] = 'Via/Piazza';
+$_lang['mapstv.housenumber'] = 'Civico';
+$_lang['mapstv.zipcode'] = 'CAP';
+$_lang['mapstv.city'] = 'Città';
+$_lang['mapstv.state'] = 'Stato';
+$_lang['mapstv.country'] = 'Paese';
+
+$_lang['mapstv.generate'] = 'Genera latitudine e longitudine e posizione il marker sulla mappa';
+
+$_lang['mapstv.latitude'] = 'Latitudine';
+$_lang['mapstv.longitude'] = 'Longitudine';
+
+$_lang['mapstv.error'] = 'Errore';
+$_lang['mapstv.error_msg'] = 'L\'indirizzo fornito è poco peciso. Controlla la posizione del marker.';
+
+$_lang['setting_mapstv.api_key'] = 'Google Maps API key';
